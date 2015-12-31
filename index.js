efe92f544e7547abeaa2d69abb954219ae466404
Kyle Corbitt <kyle@corbt.com>
2015-12-31 14:10:43 +0100

    support for records

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var transit = require('transit-immutable-js')
 
+/* Generates a redux-persist compatible transformer 
+ * based on the transit instance passed */
+function createTransformer(transitInstance) {
+  return {
+    in: function(state){
+      if(state && typeof state === 'object'){
+        return transitInstance.toJSON(state)
+      }
+      return state
+    },
+    out: function(raw){
+      if(typeof raw === 'string'){
+        return transitInstance.fromJSON(raw)
+      }
+      return raw
+    }
+  }
+}
+
+var basicTransformer = createTransformer(transit);
+
 module.exports = {
-  in: function(state){
-    if(state && typeof state === 'object'){
-      return transit.toJSON(state)
-    }
-    return state
-  },
-  out: function(raw){
-    if(typeof raw === 'string'){
-      return transit.fromJSON(raw)
-    }
-    return raw
+  in: basicTransformer.in,
+  out: basicTransformer.out,
+
+  withRecords: function(recordsArray) {
+    return createTransformer(transit.withRecords(recordsArray))
   }
 }
